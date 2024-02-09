bfbc1359a37f598a76f4dda9261599cd5792152e
bastien mioche <bastien.mioche@cgi.com>
2024-02-09 14:42:15 +0100

    2_fix:Arbitrary file read

--- routes/fileServer.ts
+++ routes/fileServer.ts
@@ -15,11 +15,11 @@ module.exports = function servePublicFiles () {
   return ({ params, query }: Request, res: Response, next: NextFunction) => {
     const file = params.file
 
-    if (!file.includes('/')) {
+    if (!file.includes('/') && !file.includes('%00')) {
       verify(file, res, next)
     } else {
       res.status(403)
-      next(new Error('File names cannot contain forward slashes!'))
+      next(new Error('File names cannot contain forward slashes and must finish by .md!'))
     }
   }
 
